3f8b1c647d5c840ba45cff98bba410851805eba4
Jack <jackjwmcgregor@gmail.com>
2024-07-11 22:44:17 +0200

    feat: 🎸 bump

--- release.config.cjs
+++ release.config.cjs
@@ -4,22 +4,24 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    [
-      "@semantic-release/npm",
-      {
-        npmPublish: true,
-        pkgRoot: ".",
-        registry: "https://npm.pkg.github.com",
-      },
-    ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    "@semantic-release/npm",
+    "@semantic-release/git",
     "@semantic-release/github",
   ],
+  preset: "angular",
+  releaseRules: [
+    { type: "breaking", scope: "*", release: "major" },
+    { type: "feat", scope: "*", release: "minor" },
+    { type: "fix", scope: "*", release: "patch" },
+    { type: "perf", scope: "*", release: "patch" },
+    { type: "docs", scope: "*", release: "patch" },
+    { type: "style", scope: "*", release: "patch" },
+    { type: "chore", scope: "*", release: "patch" },
+    { type: "refactor", scope: "*", release: "patch" },
+    { type: "test", scope: "*", release: "patch" },
+    { type: "ci", scope: "*", release: "patch" },
+  ],
+  parserOpts: {
+    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "breaking:"],
+  },
 };
