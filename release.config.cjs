c947426700c2d5181560fd76a1ba7cd9b97c5f36
Jack <jackjwmcgregor@gmail.com>
2024-07-11 22:31:20 +0200

    refactor: 💡 perpexity suggestion

--- release.config.cjs
+++ release.config.cjs
@@ -84,9 +84,18 @@ module.exports = {
       "@semantic-release/npm",
       {
         npmPublish: true,
+        pkgRoot: ".",
+        registry: "https://npm.pkg.github.com",
       },
     ],
-    "@semantic-release/git",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
     "@semantic-release/github",
   ],
 };
